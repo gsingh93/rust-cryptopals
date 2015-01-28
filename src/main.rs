a072392919a3d85539c37241c01e9a92d47d6534
Gulshan Singh <gulshan@umich.edu>
2015-01-28 12:29:26 -0500

    Implemented chal1

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-mod set1;
-
-use set1::chal1;
-
-fn main() {
-    chal1()
-}
--- src/set1.rs
+++ src/set1.rs
@@ -1,2 +1,15 @@
-pub fn chal1() {
+extern crate "rustc-serialize" as rustc_serialize;
+
+use self::rustc_serialize::base64::{ToBase64, STANDARD};
+use self::rustc_serialize::hex::FromHex;
+
+fn chal1(s: &str) -> String {
+    s.from_hex().unwrap().to_base64(STANDARD)
+}
+
+#[test]
+fn test_chal1() {
+    let s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    assert_eq!(&*chal1(s), expected);
 }
