eddb427e3f6ec85ab7f84452805836b3587c42d2
Gulshan Singh <gulshan@umich.edu>
2015-01-28 14:08:14 -0500

    Implemented chal3

--- src/set1.rs
+++ src/set1.rs
@@ -17,6 +17,28 @@ fn chal2(a: &str, b: &str) -> String {
     res.as_bytes().to_hex()
 }
 
+fn chal3(m: &str) -> Vec<(u8, String)> {
+    fn is_english(v: &[u8]) -> bool {
+        v.iter().all(|b| {
+            let c = *b as char;
+            c.is_alphabetic() || c.is_whitespace() || c.is_numeric() || ['.', '!', ',', '\''].contains(&c)
+        })
+    }
+
+    let mut res = Vec::new();
+    let b = m.from_hex().unwrap();
+    for i in 0u16..256 {
+        let mut v = Vec::new();
+        for byte in b.iter() {
+            v.push(byte ^ (i as u8));
+        }
+        if is_english(&*v) {
+            res.push((i as u8, String::from_utf8(v).unwrap().as_bytes().to_hex()));
+        }
+    }
+    res
+}
+
 #[test]
 fn test_chal1() {
     let s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
@@ -30,4 +52,14 @@ fn test_chal2() {
     let b = "686974207468652062756c6c277320657965";
     let expected = "746865206b696420646f6e277420706c6179";
     assert_eq!(chal2(a, b), expected);
+}
+
+#[test]
+fn test_chal3() {
+    let s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let expected = "436f6f6b696e67204d432773206c696b65206120706f756e64206f66206261636f6e";
+    let l = chal3(s);
+    assert_eq!(l.len(), 1);
+    let (ref k, ref m) = l[0];
+    assert_eq!(*m, expected);
 }
